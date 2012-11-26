f80d787ff1e83aca37e8b8620fdb3c861e3bf0ce
Umakanthan <cumakt@gmail.com>
2012-11-26 22:18:12 +0530

    version 1.0.1b from googlecode

---
+++ MuteVistaLib/MuteVista/MuteVista.h
@@ -0,0 +1,13 @@
+#ifndef INDLL_H
+#define INDLL_H
+
+#include <windows.h>
+
+#define WIN32_LEAN_AND_MEAN
+
+extern "C" __declspec(dllexport) void MuteAudio();
+extern "C" __declspec(dllexport) void UnMuteAudio();
+extern "C" __declspec(dllexport) void SetMuteStatus();
+extern "C" __declspec(dllexport) void GetWMPStatus();
+
+#endif
